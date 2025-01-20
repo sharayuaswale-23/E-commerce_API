fec1877da4ba41215fc5587c9a23c113918a8f28
sharayuaswale-23 <sharayuaswale123@gmail.com>
2025-01-20 12:11:52 +0530

    commit6

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-    MONGO_URI: process.env.MONGO_URI || MONGO_URI,
+    MONGO_URI: process.env.MONGO_URI,
     PORT: process.env.PORT || 5000,
     JWT_SECRET: process.env.JWT_SECRET || 'your_secret_key',
     OPENAI_API_KEY: process.env.OPENAI_API_KEY || 'your_openai_api_key'
